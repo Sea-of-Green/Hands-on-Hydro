f6fe04a5250f560b40cc329f741522878dc9f499
Alec Lomas <alec@lowmess.com>
2015-09-06 20:24:29 -0700

    Updated Photoset Layouts
    And made the gulp file better/switched to node-neat

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
-//Base Utils
+// Base Utils
 var gulp = require('gulp');
-var merge = require('merge-stream');
 var del = require('del');
-var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 // Sass / CSS
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var uncss = require('gulp-uncss');
 var csso = require('gulp-csso');
+var neat = require('node-neat');
 // HTML
 var typogr = require('gulp-typogr');
 
-gulp.task('default', ['build-theme']);
+gulp.task('default', ['build']);
 
 gulp.task('clean', function() {
   del('./dist');
@@ -27,33 +26,37 @@ gulp.task('typo', function() {
 });
 
 gulp.task('sass', ['typo'], function() {
-  var nested = gulp.src('./src/stylesheets/*.scss')
+  return gulp.src('./src/stylesheets/*.scss')
+    .pipe(sass({ includePaths: neat.includePaths }))
     .pipe(sass().on('error', sass.logError))
-    .pipe(sass({outputStyle: 'nested'}))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./dist/css'));
-  var compressed = gulp.src('./src/stylesheets/*.scss')
-    .pipe(sass())
-    // Will bring this back, have to split the stylesheets so posts aren't affected
     //.pipe(uncss({
-    //    html: ['./dist/**/*.html']
+    //  html: ['./dist/**/*.html']
     //}))
     .pipe(autoprefixer())
     .pipe(csso())
-    .pipe(rename(function(path) {
-      path.basename += '.min';
-      path.extname = '.css';
-    }))
     .pipe(gulp.dest('./dist/css'));
-  return merge(nested, compressed);
 });
 
-gulp.task('build-theme', ['typo', 'sass'], function() {
-  return gulp.src(['./dist/html/head.html', './dist/css/main.min.css', './dist/html/svg.html', './dist/html/body.html'])
+gulp.task('sass:debug', ['typo'], function() {
+  return gulp.src('./src/stylesheets/*.scss')
+    .pipe(sass({
+      outputStyle: 'nested',
+      includePaths: neat.includePaths
+    }))
+    .pipe(sass().on('error', sass.logError))
+    //.pipe(uncss({
+    //  html: ['./dist/**/*.html']
+    //}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('build', ['typo', 'sass'], function() {
+  return gulp.src(['./dist/html/head.html', './dist/css/main.css', './dist/html/svg.html', './dist/html/body.html'])
     .pipe(concat('theme.html'))
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('watch', ['build-theme'], function () {
-  gulp.watch(['./src/stylesheets/*.scss', './src/stylesheets/**/*.scss', './src/*.html'], ['build-theme']);
+gulp.task('watch', ['build'], function () {
+  gulp.watch(['./src/stylesheets/*.scss', './src/stylesheets/**/*.scss', './src/*.html'], ['build']);
 });
