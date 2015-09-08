a1903072d2ba25e0e498b427ce1c6077ee38ca81
Alec Lomas <alec@lowmess.com>
2015-09-07 21:24:54 -0700

    Update Gulpfile
    * Changed src markup location
    * Updated CSS minification
    * Updated Gulpfile to auto-delete compiled HTML & CSS on build

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ var del = require('del');
 var concat = require('gulp-concat');
 // Sass / CSS
 var sass = require('gulp-sass');
+var neat = require('node-neat');
 var autoprefixer = require('gulp-autoprefixer');
 var uncss = require('gulp-uncss');
+var cmq = require('gulp-combine-media-queries');
 var csso = require('gulp-csso');
-var neat = require('node-neat');
 // HTML
 var typogr = require('gulp-typogr');
 
@@ -18,10 +19,15 @@ gulp.task('clean', function() {
 });
 
 gulp.task('typo', function() {
-  return gulp.src('./src/*.html')
+  return gulp.src('./src/markup/*.html')
     .pipe(typogr({
       only: ['amp', 'widont', 'smartypants']
     }))
+    .pipe(gulp.dest('./dist/html/'));
+});
+
+gulp.task('typo:debug', function() {
+  return gulp.src('./src/markup/*.html')
     .pipe(gulp.dest('./dist/html/'));
 });
 
@@ -29,10 +35,12 @@ gulp.task('sass', ['typo'], function() {
   return gulp.src('./src/stylesheets/*.scss')
     .pipe(sass({ includePaths: neat.includePaths }))
     .pipe(sass().on('error', sass.logError))
-    //.pipe(uncss({
-    //  html: ['./dist/**/*.html']
-    //}))
+    .pipe(uncss({
+      ignore: [/(\.post|\.text|\.photo|\.panorama|\.photoset|\.quote|\.link|\.chat|\.audio|\.video)/, /(\.post|\.text|\.photo|\.panorama|\.photoset|\.quote|\.link|\.chat|\.audio|\.video)+.+?(?=\s{)/],
+      html: ['./dist/**/*.html']
+    }))
     .pipe(autoprefixer())
+    .pipe(cmq())
     .pipe(csso())
     .pipe(gulp.dest('./dist/css'));
 });
@@ -44,17 +52,23 @@ gulp.task('sass:debug', ['typo'], function() {
       includePaths: neat.includePaths
     }))
     .pipe(sass().on('error', sass.logError))
-    //.pipe(uncss({
-    //  html: ['./dist/**/*.html']
-    //}))
+    .pipe(uncss({
+      ignore: [/(\.post|\.text|\.photo|\.panorama|\.photoset|\.quote|\.link|\.chat|\.audio|\.video)/, /(\.post|\.text|\.photo|\.panorama|\.photoset|\.quote|\.link|\.chat|\.audio|\.video)+.+?(?=\s{)/],
+      html: ['./dist/**/*.html']
+    }))
     .pipe(autoprefixer())
+    .pipe(cmq())
     .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('build', ['typo', 'sass'], function() {
+gulp.task('theme', ['sass'], function() {
   return gulp.src(['./dist/html/head.html', './dist/css/main.css', './dist/html/svg.html', './dist/html/body.html'])
     .pipe(concat('theme.html'))
     .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build', ['clean', 'theme'], function() {
+  del(['./dist/html', './dist/css']);
 });
 
 gulp.task('watch', ['build'], function () {
